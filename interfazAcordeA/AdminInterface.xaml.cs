2ed851ca4a05bbfe7f73007c6f865bebe5abab35
carloserochav <g14ada02@hackademy.mx>
2022-09-19 16:01:57 -0700

    Primer commit

---
+++ interfazAcordeA/AdminInterface.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace interfazAcordeA
+{
+    /// <summary>
+    /// Lógica de interacción para AdminInterface.xaml
+    /// </summary>
+    public partial class AdminInterface : Window
+    {
+        public AdminInterface()
+        {
+            InitializeComponent();
+        }
+    }
+}
